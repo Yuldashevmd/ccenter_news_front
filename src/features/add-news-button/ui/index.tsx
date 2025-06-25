1a0df9cd24e36501ea47f024950d67a1ce613d4f
Muhammad <muxammad_205@mail.ru>
2025-06-25 17:17:03 +0500

    feat: Enhance AddNewsButton with icon and improved layout

--- src/features/add-news-button/ui/index.tsx
+++ src/features/add-news-button/ui/index.tsx
@@ -1,6 +1,9 @@
+import { PlusIcon } from 'lucide-react';
+
 export const AddNewsButton = () => {
   return (
-    <button title='Add news' className="w-fit ml-auto text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 focus:outline-none">
+    <button title='Add news' className="w-fit flex items-center gap-2 ml-auto text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 focus:outline-none">
+      <PlusIcon className="w-5 h-5" />
       Add News
     </button>
   );
