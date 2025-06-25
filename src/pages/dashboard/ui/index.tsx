c0b9a0bdb033ba0288d6fe5e91ada5c1f6512b91
Muhammad <muxammad_205@mail.ru>
2025-06-25 17:34:40 +0500

    feat: Implement pagination functionality in DashboardPage and enhance DashboardTable with dynamic data

--- src/pages/dashboard/ui/index.tsx
+++ src/pages/dashboard/ui/index.tsx
@@ -1,8 +1,20 @@
 import { AddNewsButton } from 'features/add-news-button';
 import { Pagination } from 'features/pagination';
+import { useState } from 'react';
 import { DashboardTable } from 'widgets/dashboard-table';
 
+const data = [
+  { id: 1, title: 'Sample Title', type: 'Sample Type', date: '2023-01-01' },
+  { id: 2, title: 'Sample Title', type: 'Sample Type', date: '2023-01-01' },
+  { id: 3, title: 'Sample Title', type: 'Sample Type', date: '2023-01-01' },
+];
 export const DashboardPage = () => {
+  const [limit,setLimit]=useState<number>(10)
+
+  const onLimitChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLimit(Number(e.target.value));
+  }
+
   return (
     <main className="min-h-full rounded-xl shadow-md max-w-7xl mx-auto mt-24 p-8 bg-white flex flex-col items-center">
       <div className="flex flex-col items-center mb-8">
@@ -12,8 +24,8 @@ export const DashboardPage = () => {
         </p>
       </div>
       <AddNewsButton />
-      <DashboardTable />
-      <Pagination total={0}/>
+      <DashboardTable rows={data}/>
+      <Pagination total={0} limit={limit} onChange={onLimitChange}/>
     </main>
   );
 };
--- src/widgets/dashboard-table/ui/index.tsx
+++ src/widgets/dashboard-table/ui/index.tsx
@@ -1,4 +1,5 @@
 import { Pencil, Trash, View } from 'lucide-react';
+import { FC } from 'react';
 
 const columns = [
   { title: 'Title', field: 'title' },
@@ -7,13 +8,13 @@ const columns = [
   { title: 'Actions', field: 'actions' },
 ];
 
-const rows = [
-  { id: 1, title: 'Sample Title', type: 'Sample Type', date: '2023-01-01' },
-  { id: 2, title: 'Sample Title', type: 'Sample Type', date: '2023-01-01' },
-  { id: 3, title: 'Sample Title', type: 'Sample Type', date: '2023-01-01' },
-];
+interface IProps {
+  rows: any[];
+}
 
-export const DashboardTable = () => {
+export const DashboardTable: FC<IProps> = (props) => {
+  const { rows } = props;
+
   return (
     <table className="table w-full border border-gray-200">
       <thead className="bg-gray-100 border-b border-gray-200">
@@ -33,13 +34,13 @@ export const DashboardTable = () => {
             <td className="px-4 py-2">{row.type}</td>
             <td className="px-4 py-2">{row.date}</td>
             <td className="px-4 py-2">
-              <span className="text-blue-600 hover:underline" title='Edit'>
+              <span className="text-blue-600 hover:underline" title="Edit">
                 <Pencil className="inline w-5 h-5 text-orange-400 cursor-table" />
               </span>
-              <span className="text-red-600 hover:underline ml-2" title='Delete'>
+              <span className="text-red-600 hover:underline ml-2" title="Delete">
                 <Trash className="inline w-5 h-5 text-red-400 cursor-table" />
               </span>
-              <span className="text-green-600 hover:underline ml-2" title='View'>
+              <span className="text-green-600 hover:underline ml-2" title="View">
                 <View className="inline w-5 h-5 text-blue-400 cursor-table" />
               </span>
             </td>
